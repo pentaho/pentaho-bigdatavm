c787d7d3e1a3e99109f0a17bad2950e165a2df2f
Robert Woodward <rwoodward@users.noreply.github.com>
2017-06-20 13:43:08 -0400

    Update magicwrapper.sh

--- magicwrapper.sh
+++ magicwrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## This file will pull down the magic script and execute it. 
+## This file will pull down the magic script and execute it. Create a launcher for this on the desktop and mark as executable.
 
 BASEDIR=$(dirname $0)
 # Is there another copy of this file already in place? kill it!
