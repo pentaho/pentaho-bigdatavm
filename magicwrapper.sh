273a40bcf290af303b7c83d222002ac7ac8c8199
Robert Woodward <rwoodward@users.noreply.github.com>
2017-06-20 13:41:50 -0400

    Create magicwrapper.sh
    Use this file as the executable to download and run the magicscript file

---
+++ magicwrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+# Is there another copy of this file already in place? kill it!
+if [ -f $BASEDIR/magicScript.sh ]; then
+rm -rf $BASEDIR/magicScript.sh
+fi
+/usr/bin/wget https://raw.githubusercontent.com/pentaho/pentaho-bigdatavm/master/magicScript.sh
+chmod +x $BASEDIR/magicScript.sh
+$BASEDIR/magicScript.sh
